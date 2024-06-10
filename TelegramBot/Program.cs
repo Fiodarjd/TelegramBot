b1359e549064785e33dcbe53f74339782113ea6d
Администратор <@SERVER>
2024-06-10 12:53:08 +0300

    Добавьте файлы проекта.

---
+++ TelegramBot/Program.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Telegram.Bots;
+using Telegram.Bots.Extensions.Polling;
+
+HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
+
+builder.Configuration.AddUserSecrets<Program>();
+string token = builder.Configuration["Token"];
+builder.Services.AddBotClient(token);
+builder.Services.AddPolling<UpdateHandler>();
+var provider = builder.Services.BuildServiceProvider();
+
+
+IHost host = builder.Build();
+host.Run();
---
+++ TelegramBot/UpdateHandler.cs
@@ -0,0 +1,32 @@
+﻿using Telegram.Bots;
+using Telegram.Bots.Extensions.Polling;
+using Telegram.Bots.Requests;
+using Telegram.Bots.Types;
+ 
+internal class UpdateHandler : IUpdateHandler
+{
+    public async Task HandleAsync(IBotClient bot, Update update, CancellationToken token)
+    {
+        if (update is MessageUpdate u)
+        {
+            SendText request = new( u.Data.Chat.Id,  "Hello!");
+
+            Response<TextMessage> response = await bot.HandleAsync(request);
+
+            if (response.Ok)
+            {
+                TextMessage message = response.Result;
+
+                Console.WriteLine(message.Id);
+                Console.WriteLine(message.Text);
+                Console.WriteLine(message.Date.ToString("G"));
+            }
+            else
+            {
+                Failure failure = response.Failure;
+
+                Console.WriteLine(failure.Description);
+            }
+        }
+    }
+}
